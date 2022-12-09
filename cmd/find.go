e22e5b415d54a15c38c2660ed76a61a21c3fc12b
Mischa Thompson <mischabear@gmail.com>
2022-12-09 15:24:59 -0800

    Update find long message

--- cmd/find.go
+++ cmd/find.go
@@ -13,7 +13,7 @@ func init() {
 var findCmd = &cobra.Command{
 	Use:   "find",
 	Short: "Finds and displays goto links without opening in the browser",
-	Long:  "Finds and displays the link for the goto label specified. If no label is supplied it will display all goto labels in the store",
+	Long:  "Finds and displays the link for the goto label specified. If no label is supplied it will display all goto labels in the store. If there is a large number, recommended to pipe the output to less or grep",
 	Run: func(cmd *cobra.Command, args []string) {
 		switch len(args) {
 		case 0:
