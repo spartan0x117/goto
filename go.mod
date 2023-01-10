333c9b266593aca620acb0fd0d5b32a145a3c217
Mischa Thompson <mischabear@gmail.com>
2023-01-09 18:19:12 -0800

    Add a handler for adding go links

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.19
 
 require (
 	github.com/go-git/go-git/v5 v5.5.0
+	github.com/labstack/echo/v4 v4.10.0
+	github.com/labstack/gommon v0.4.0
 	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8
 	github.com/spf13/cobra v1.6.1
 	github.com/spf13/viper v1.14.0
@@ -26,8 +28,6 @@ require (
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
 	github.com/kevinburke/ssh_config v1.2.0 // indirect
-	github.com/labstack/echo/v4 v4.10.0 // indirect
-	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
--- pkg/server/http.go
+++ pkg/server/http.go
@@ -3,24 +3,53 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"net/url"
+	"strings"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
 	"github.com/spartan0x117/goto/pkg/storage"
 )
 
 func NewServer(s storage.Storage) *echo.Echo {
 	e := echo.New()
+	e.Logger.SetLevel(log.INFO)
+	//e.Use(middleware.Logger())
+
+	addHandler := func(c echo.Context) error {
+		e.Logger.Info("inside 'addHandler'")
+		if c.Request().Method == http.MethodPost {
+			label, link := c.FormValue("label"), c.FormValue("url")
+			err := s.AddLink(label, link)
+			if err != nil {
+				return c.String(http.StatusBadRequest, "could not add goto link...")
+			}
+			return c.String(http.StatusAccepted, fmt.Sprintf("added '%s: %s'", label, link))
+		}
+		return c.File("./pkg/server/add_form.html")
+	}
+	e.GET("/add", addHandler)
+	e.POST("/add", addHandler)
+
 	gotoHandler := func(c echo.Context) error {
+		e.Logger.Info("inside 'gotoHandler'")
 		label := c.Param("label")
 		if label == "" {
 			return c.String(http.StatusOK, "Please enter a label: go/<label>")
 		}
 		link := s.GetLinkForLabel(label)
+		if !strings.HasPrefix(link, "https://") && !strings.HasPrefix(link, "http://") {
+			link = "https://" + link
+		}
+		u, err := url.Parse(link)
+		if err != nil {
+			return c.String(http.StatusInternalServerError, fmt.Sprintf("error parsing url: %s", link))
+		}
 		if link == "" {
 			return c.String(http.StatusNotFound, fmt.Sprintf("Label '%s' not found", label))
 		}
 
-		return c.Redirect(http.StatusFound, link)
+		return c.Redirect(http.StatusFound, u.String())
 	}
 
 	e.GET("/:label", gotoHandler)
